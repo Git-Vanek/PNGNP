c1080e6361fb401761448dad57aaa88a4d4be170
Komkov Ivan <ki16082005@gmail.com>
2024-12-24 16:26:02 +0300

    Добавлена функция сохранеия фотографии.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.example._pngnp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires java.desktop;
     requires org.apache.logging.log4j;
-
 
     opens org.example._pngnp to javafx.fxml;
     exports org.example._pngnp;
--- src/main/java/org/example/_pngnp/MainController.java
+++ src/main/java/org/example/_pngnp/MainController.java
@@ -1,9 +1,9 @@
 package org.example._pngnp;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
@@ -11,6 +11,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MainController {
     private ImageModel model;
@@ -65,6 +69,41 @@ public class MainController {
             model.loadImage(originalPath);
             imageView.setImage(model.getImage());
         }
+    }
+
+    @FXML
+    private void saveImage() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Image File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("PNG Files", "*.png"),
+                new FileChooser.ExtensionFilter("JPEG Files", "*.jpg"),
+                new FileChooser.ExtensionFilter("BMP Files", "*.bmp")
+        );
+        File file = fileChooser.showSaveDialog(primaryStage);
+        if (file != null) {
+            try {
+                javafx.scene.image.Image image = model.getImage();
+                if (image != null) {
+                    BufferedImage bufferedImage = SwingFXUtils.fromFXImage(image, null);
+                    String format = getFileExtension(file);
+                    ImageIO.write(bufferedImage, format, file);
+                } else {
+                    System.out.println("No image to save.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getFileExtension(File file) {
+        String name = file.getName();
+        int lastIndexOf = name.lastIndexOf(".");
+        if (lastIndexOf == -1) {
+            return "png"; // default format
+        }
+        return name.substring(lastIndexOf + 1);
     }
 
     @FXML
